b06991f21fb64a08baa0a677551e36eb4bb74a00
Tesfa Mk <natnaelmalike@gmail.com>
2023-09-22 01:16:30 +0300

    final

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -63,7 +63,7 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement
     this.currentOperandTextElement = currentOperandTextElement
-    //this.clear()
+    this.clear()
   }
 
   clear() {
